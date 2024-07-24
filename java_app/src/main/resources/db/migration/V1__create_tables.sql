747c7fe2ea552bae39283d7245709410467c2082
Jo Youngjae <joyoungjae0401@gmail.com>
2024-07-24 18:49:48 +0900

    refactor: 초기 마이그레이션 파일 수정

--- java_app/src/main/resources/db/migration/V1__create_tables.sql
+++ java_app/src/main/resources/db/migration/V1__create_tables.sql
@@ -5,8 +5,8 @@ CREATE TABLE `users` (
     password varchar(255) NOT NULL COMMENT '비밀번호',
     phone_number varchar(20) NOT NULL COMMENT '전화번호',
     `role` varchar(20) NOT NULL COMMENT '권한',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '등록일',
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL COMMENT '수정일',
+    created_at TIMESTAMP NOT NULL COMMENT '등록일',
+    updated_at TIMESTAMP NOT NULL COMMENT '수정일',
     deleted_at TIMESTAMP NULL COMMENT '삭제일'
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE `products` (
     id BIGINT UNSIGNED auto_increment PRIMARY KEY NOT NULL,
     name varchar(100) NOT NULL COMMENT '상품명',
     price int NOT NULL COMMENT '상품금액',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '등록일',
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL COMMENT '수정일',
+    created_at TIMESTAMP NOT NULL COMMENT '등록일',
+    updated_at TIMESTAMP NOT NULL COMMENT '수정일',
     deleted_at TIMESTAMP NULL COMMENT '삭제일'
 );
