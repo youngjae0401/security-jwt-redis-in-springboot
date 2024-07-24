59c881059edf4460715f70890cb125a3d1d59a03
Jo Youngjae <joyoungjae0401@gmail.com>
2024-07-24 17:00:54 +0900

    feat: 초기 마이그레이션 파일 추가

---
+++ java_app/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `users` (
+    id BIGINT UNSIGNED auto_increment PRIMARY KEY NOT NULL,
+    email varchar(100) NOT NULL COMMENT '이메일',
+    name varchar(30) NOT NULL COMMENT '이름',
+    password varchar(255) NOT NULL COMMENT '비밀번호',
+    phone_number varchar(20) NOT NULL COMMENT '전화번호',
+    `role` varchar(20) NOT NULL COMMENT '권한',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '등록일',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL COMMENT '수정일',
+    deleted_at TIMESTAMP NULL COMMENT '삭제일'
+);
+
+CREATE TABLE `products` (
+    id BIGINT UNSIGNED auto_increment PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL COMMENT '상품명',
+    price int NOT NULL COMMENT '상품금액',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '등록일',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL COMMENT '수정일',
+    deleted_at TIMESTAMP NULL COMMENT '삭제일'
+);
